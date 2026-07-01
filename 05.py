3b6b3f21d2e2d07ffca39e4a1c05ba702fb3f67b
Your Name <you@example.com>
2026-07-01 20:03:09 +0800

    add 05

---
+++ 05.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+a1=tk.Tk()
+a1.title('小王')
+#获取用户分辨率,这样设置更合理
+a2=a1.maxsize()
+print(a2)
+k,g=a2
+a1.geometry(f'{int(k/2)}x{int(g/2)}')
+a1.resizable(False,False)
+#设置窗口图标
+a1.iconbitmap('logo.ico')
+
+#设置窗口制定
+a1.attributes('-topmost',True)
+
+
+#结束语句
+a1.mainloop()

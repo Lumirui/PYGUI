f1d787c7d273e3471ad60679b42adcaf44b38955
Lumirui <3831983696@qq.com>
2026-07-01 21:41:35 +0800

    Add files via upload

---
+++ 06.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+a1=tk.Tk()
+a1.title('小王')
+#获取用户分辨率,这样设置更合理
+a2=a1.maxsize()
+print(a2)
+k,g=a2
+a1.geometry(f'{int(k/2)}x{int(g/2)}')
+a1.resizable(False,False)
+#设置窗口图标
+a1.iconbitmap('logo.ico')
+
+#标签组件
+a2=tk.Label(a1,text='小王',font=('黑体',20),fg='blue',bg='brown')
+a2.pack()
+
+#结束语句
+a1.mainloop()

55c5af52813fdb053198342e192f31f6cb8233fc
Lumirui <3831983696@qq.com>
2026-07-02 21:36:29 +0800

    Add files via upload

---
+++ 09.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+a1=tk.Tk()
+a1.title('登录页')
+#获取用户分辨率,这样设置更合理
+a2=a1.maxsize()
+print(a2)
+k,g=a2
+a1.geometry('500x400')
+a1.resizable(False,False)
+#设置窗口图标
+a1.iconbitmap('logo.ico')
+
+tk.Label(a1,text='账号:',font=('黑体',26)).place(x=50,y=100)
+tk.Label(a1,text='密码:',font=('黑体',26)).place(x=50,y=180)
+#创建字符串变量
+s1=tk.StringVar()
+#预设文本,不是placeholder
+s1.set('请输入账号')
+s2=tk.StringVar()
+s2.set('请输入密码')
+
+#输入框组件
+tk.Entry(a1,textvariable=s1,width=15,font=('黑体',26)).place(x=150,y=100)
+tk.Entry(a1,textvariable=s2,width=15,font=('黑体',26)).place(x=150,y=180)
+def dl():
+  # print('小王')
+  #字符串变量获取
+  print(s1.get())
+  print(s2.get())
+
+tk.Button(a1,text='登录',command=dl,font=('黑体',26),width=10).place(x=150,y=280)
+#结束语句
+a1.mainloop()
